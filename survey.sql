7f5a1c397f06664b8b1869514156fb2f6c636091
Alexis Cacayuran <69415093+thealxs1@users.noreply.github.com>
2021-04-19 20:25:06 +0800

    Update survey.sql

--- survey.sql
+++ survey.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:8111
--- Generation Time: Apr 19, 2021 at 06:53 AM
+-- Generation Time: Apr 19, 2021 at 02:23 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -20,27 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `test`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `demographics`
---
-
-CREATE TABLE `demographics` (
-  `id` int(11) NOT NULL,
-  `up_mail` text NOT NULL,
-  `full_name` text DEFAULT NULL,
-  `student_num` text DEFAULT NULL,
-  `degree_program` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `demographics`
---
-
-INSERT INTO `demographics` (`id`, `up_mail`, `full_name`, `student_num`, `degree_program`) VALUES
-(1, 'aacacayuran@up.edu.ph', 'Alexis A. Cacayuran', '2018-03829', 'BS Computer Science');
 
 -- --------------------------------------------------------
 
@@ -106,51 +85,9 @@ INSERT INTO `questionnaire` (`question_id`, `question`, `classification`) VALUES
 (23, 'Did you receive or attend any career counseling conducted within the university?\r\n', 'Career Plans'),
 (24, 'Will you be willing to be contacted for a follow-up interview?', 'Consent for a follow-up interview');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `respondents`
---
-
-CREATE TABLE `respondents` (
-  `id` int(11) NOT NULL,
-  `question_id` int(5) NOT NULL,
-  `rate` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `respondents`
---
-
-INSERT INTO `respondents` (`id`, `question_id`, `rate`) VALUES
-(1, 1, 4),
-(1, 2, 5),
-(1, 3, 4),
-(1, 4, 5),
-(1, 5, 4),
-(1, 6, 4),
-(1, 7, 4),
-(1, 8, 4),
-(1, 9, 3),
-(3, 1, 3),
-(3, 2, 4),
-(3, 3, 3),
-(3, 4, 4),
-(3, 5, 4),
-(3, 6, 4),
-(3, 7, 3),
-(3, 8, 4),
-(3, 9, 4);
-
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `demographics`
---
-ALTER TABLE `demographics`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `login`
@@ -163,13 +100,6 @@ ALTER TABLE `login`
 --
 ALTER TABLE `questionnaire`
   ADD PRIMARY KEY (`question_id`);
-
---
--- Indexes for table `respondents`
---
-ALTER TABLE `respondents`
-  ADD KEY `question_id` (`question_id`),
-  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -186,22 +116,6 @@ ALTER TABLE `login`
 --
 ALTER TABLE `questionnaire`
   MODIFY `question_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `demographics`
---
-ALTER TABLE `demographics`
-  ADD CONSTRAINT `demographics_ibfk_1` FOREIGN KEY (`id`) REFERENCES `login` (`id`);
-
---
--- Constraints for table `respondents`
---
-ALTER TABLE `respondents`
-  ADD CONSTRAINT `respondents_ibfk_1` FOREIGN KEY (`id`) REFERENCES `demographics` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
